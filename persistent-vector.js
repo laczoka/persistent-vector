883e8f97f03071ddaee2331f4a8300e1b33db762
Laszlo Toeroek <ltorokjr@gmail.com>
2012-03-06 11:00:31 +0100

    implement non-recursive pushTail and newPath, newPath now correctly inlines on V8

--- persistent-vector.js
+++ persistent-vector.js
@@ -1,199 +1,220 @@
 var EMPTY_NODE = new Array(32);
 
 function PersistentVector (cnt, shift, root, tail) {
-  this.cnt = cnt;
-  this.shift = shift;
-  this.root = root;
-  this.tail = tail;
+    this.cnt = cnt;
+    this.shift = shift;
+    this.root = root;
+    this.tail = tail;
 }
 
 PersistentVector.prototype = {
-  /* should inline */
-  tailOff: function() {
-    if(this.cnt < 32) {
-      return 0;
-    }
-    return ((this.cnt-1) >>> 5) << 5;
-  },
-  arrayFor: function(i) {
-    if(i >= 0 && i < this.cnt) {
-      if(i >= this.tailOff()) {
-        return this.tail;
-      }
-      var node = this.root;
-      for(var level = this.shift; level > 0; level -= 5) {
-        node = node[(i >>> level) & 0x01f];
-      }
-      return node;
-    }
-    throw new Error("Index out of bounds!");
-  },
-  nth_1: function(i) {
-    var node = this.arrayFor(i);
-    return node[i & 0x01f];
-  },
-  nth_2: function(i, notFound) {
-    if(i >= 0 && i < this.cnt) {
-      return this.nth_1(i);
-    }
-    return notFound;
-  },
-  assocN: function(i, val) {
-    if(i >= 0 && i < this.cnt) {
-      if(i >= this.tailOff()) {
-        newtail = this.tail.slice(0);
-        newtail[i & 0x01f] = val;
-        return new PersistentVector(this.cnt, this.shift, this.root, newtail);
-      }
-      return new PersistentVector(this.cnt, this.shift, this.doAssoc(this.shift, this.root, i, val), this.tail);
-    }
-    if(i === this.cnt) {
-      return this.cons(val);
-    }
-    throw new Error("Index out of bounds!");
-  },
-  doAssoc: function(level, node, i, val) {
-    var ret = node.slice(0);
-    if(level === 0) {
-      ret[i & 0x01f] = val;
-    } else {
-      var subidx = (i >>> level) & 0x01f;
-      ret[subidx] = this.doAssoc(level-5, node[subidx], i, val);
-    }
-    return ret;
-  },
-  count: function() {
-    return this.cnt;
-  },
-  cons: function(val) {
-    var i = this.cnt;
-    if((this.cnt - this.tailOff()) < 32) {
-      var newtail = this.tail.slice(0);
-      newtail.push(val);
-      return new PersistentVector(this.cnt+1, this.shift, this.root, newtail);
-    }
-    var newroot,
-        tailnode = this.tail,
-        newshift = this.shift;
-    if((this.cnt >>> 5) > (1 << this.shift)) {
-      newroot = EMPTY_NODE.slice(0);
-      newroot[0] = this.root;
-      newroot[1] = this.newPath(this.shift, tailnode);
-      newshift += 5;
-    } else {
-      newroot = this.pushTail(this.shift, this.root, tailnode);
-    }
-    return new PersistentVector(this.cnt+1, newshift, newroot, [val]);
-  },
-  pushTail: function(level, parent, tailnode) {
-    var subidx = ((this.cnt-1) >>> level) & 0x01f,
-        ret = parent.slice(0),
-        nodeToInsert;
-    if(level === 5) {
-      nodeToInsert = tailnode;
-    } else {
-      var child = parent[subidx];
-      nodeToInsert = (child != null) ? this.pushTail(level-5, child, tailnode) : this.newPath(level-5, tailnode);
-    }
-    ret[subidx] = nodeToInsert;
-    return ret;
-  },
-  newPath: function(level, node) {
-    /* probably can loop in place to allow for inlining self call */
-    if(level === 0) {
-      return node;
-    }
-    var ret = EMPTY_NODE.slice(0);
-    ret[0] = this.newPath(level-5, node);
-    return ret;
-  },
-  empty: function() {
-    return EMPTY;
-  },
-  pop: function() {
-    if(this.cnt == 0) {
-      throw new Error("Can't pop empty vector");
-    }
-    if(this.cnt-this.tailOff() > 1) {
-      var newTail = this.tail.slice(0, this.tail.length-1);
-      return new PersistentVector(this.cnt-1, this.shift, this.root, newTail);
-    }
-    var newtail = this.arrayFor(cnt-2),
-        newroot = this.popTail(this.shift, this.root),
-        newshift = this.shift;
-    if(newroot === null) {
-      newroot = EMPTY_NODE;
-    }
-    if(shift > 5 && newroot[1] == null) {
-      newroot = newroot[0];
-      newshift -= 5;
-    }
-    return new PersistentVector(this.cnt-1, newshift, newroot, newtail);
-  },
-  popTail: function(level, node) {
-    var subidx = ((cnt-2) >>> level) & 0x01f;
-    if(level > 5) {
-      var newchild = this.popTail(level-5, node[subidx]);
-      if(newchild == null && subidx == 0) {
-        return null;
-      } else {
+    /* should inline */
+    tailOff: function() {
+        if(this.cnt < 32) {
+            return 0;
+        }
+        return ((this.cnt-1) >>> 5) << 5;
+    },
+    arrayFor: function(i) {
+        if(i >= 0 && i < this.cnt) {
+            if(i >= this.tailOff()) {
+                return this.tail;
+            }
+            var node = this.root;
+            for(var level = this.shift; level > 0; level -= 5) {
+                node = node[(i >>> level) & 0x01f];
+            }
+            return node;
+        }
+        throw new Error("Index out of bounds!");
+    },
+    nth_1: function(i) {
+        var node = this.arrayFor(i);
+        return node[i & 0x01f];
+    },
+    nth_2: function(i, notFound) {
+        if(i >= 0 && i < this.cnt) {
+            return this.nth_1(i);
+        }
+        return notFound;
+    },
+    assocN: function(i, val) {
+        if(i >= 0 && i < this.cnt) {
+            if(i >= this.tailOff()) {
+                newtail = this.tail.slice(0);
+                newtail[i & 0x01f] = val;
+                return new PersistentVector(this.cnt, this.shift, this.root, newtail);
+            }
+            return new PersistentVector(this.cnt, this.shift, this.doAssoc(this.shift, this.root, i, val), this.tail);
+        }
+        if(i === this.cnt) {
+            return this.cons(val);
+        }
+        throw new Error("Index out of bounds!");
+    },
+    doAssoc: function(level, node, i, val) {
         var ret = node.slice(0);
+        if(level === 0) {
+            ret[i & 0x01f] = val;
+        } else {
+            var subidx = (i >>> level) & 0x01f;
+            ret[subidx] = this.doAssoc(level-5, node[subidx], i, val);
+        }
         return ret;
-      }
-    } else if(subidx === 0) {
-      return null;
-    } else {
-      var ret = this.node.slice(0);
-      array[subidx] = null;
-      return ret;
+    },
+    count: function() {
+        return this.cnt;
+    },
+    cons: function(val) {
+        var i = this.cnt;
+        if((this.cnt - this.tailOff()) < 32) {
+            var newtail = this.tail.slice(0);
+            newtail.push(val);
+            return new PersistentVector(this.cnt+1, this.shift, this.root, newtail);
+        }
+        var newroot,
+            tailnode = this.tail,
+            newshift = this.shift;
+        if((this.cnt >>> 5) > (1 << this.shift)) {
+            newroot = EMPTY_NODE.slice(0);
+            newroot[0] = this.root;
+            newroot[1] = this.newPath(this.shift, tailnode);
+            newshift += 5;
+        } else {
+            newroot = this.pushTail(this.shift, this.root, tailnode);
+        }
+        return new PersistentVector(this.cnt+1, newshift, newroot, [val]);
+    },
+
+    /*
+     * | parent.slice(0)[ subidx1 <- tailnode ] if level1 == 5
+     * | parent.slice(0)[subidx1 <- newPath1 ] if level1 != 5 && child1 == null
+     * | parent.slice(0)[subidx1 <- parent2.slice(0)[subidx2 <- tailnode ] ] if level1 !=5 && child1 != null && level2 == 5
+     * | parent.slice(0)[subidx1 <- parent2.slice(0)[subidx2 <- newPath2 ] ] ...
+     * | parent.slice(0)[subidx1 <- parent2.slice(0)[subidx2 <- parent3.slice(0)[subidx3 <- tailnode ] ] ...
+     * | parent.slice(0)[subidx1 <- parent2.slice(0)[subidx2 <- parent3.slice(0)[subidx3 <- newPath3 ] ] ...
+     */
+
+    pushTail: function(level, parent, tailnode) {
+        var llevel = level,
+            lsubidx = ((this.cnt-1) >>> level) & 0x01f,
+            lchild,
+            lparent = parent,
+            ret = parent.slice(0),
+            lret = ret;
+
+        do {
+            if (llevel === 5) {
+                lret[lsubidx] = tailnode; break;
+            } else {
+                lchild = lparent[lsubidx];
+                if (lchild == null) {
+                    lret[lsubidx] = this.newPath(level-5, tailnode);
+                    break;
+                } else {
+                    lret[lsubidx] = lparent.slice(0);
+
+                    lret = lret[lsubidx];
+                    llevel -= 5;
+                    lparent = lchild;
+                    lsubidx = ((this.cnt-1) >>> level) & 0x01f;
+                }
+            }
+
+        } while (1);
+        return ret;
+    },
+
+    newPath: function(level, node) {
+        var ret = node, embed;
+        while (level>0) {
+            embed = ret;
+            ret = EMPTY_NODE.slice(0);
+            ret[0] = embed;
+            level -= 5;
+        }
+        return ret;
+    },
+
+    empty: function() {
+        return EMPTY;
+    },
+
+    pop: function() {
+        if(this.cnt == 0) {
+            throw new Error("Can't pop empty vector");
+        }
+        if(this.cnt-this.tailOff() > 1) {
+            var newTail = this.tail.slice(0, this.tail.length-1);
+            return new PersistentVector(this.cnt-1, this.shift, this.root, newTail);
+        }
+        var newtail = this.arrayFor(cnt-2),
+            newroot = this.popTail(this.shift, this.root),
+            newshift = this.shift;
+        if(newroot === null) {
+            newroot = EMPTY_NODE;
+        }
+        if(shift > 5 && newroot[1] == null) {
+            newroot = newroot[0];
+            newshift -= 5;
+        }
+        return new PersistentVector(this.cnt-1, newshift, newroot, newtail);
+    },
+    popTail: function(level, node) {
+        var subidx = ((cnt-2) >>> level) & 0x01f;
+        if(level > 5) {
+            var newchild = this.popTail(level-5, node[subidx]);
+            if(newchild == null && subidx == 0) {
+                return null;
+            } else {
+                var ret = node.slice(0);
+                return ret;
+            }
+        } else if(subidx === 0) {
+            return null;
+        } else {
+            var ret = this.node.slice(0);
+            array[subidx] = null;
+            return ret;
+        }
     }
-  }
 };
 
 var EMPTY = new PersistentVector(0, 5, EMPTY_NODE, []);
 
+
 /*
-function time(f) {
-  var start = new Date();
-  f();
-  console.log((new Date())-start);
+function time(f, msg) {
+    var start = new Date();
+    var res = f();
+    print(msg + " " + ((new Date())-start) + " ms");
+    return res;
 }
 
-time(function() {
-  for(var i = 0; i < 1e6; i++) {
-    new PersistentVector(0, 5, EMPTY_NODE, []);
-  }
-});
+function build_array() {
+    var a = [];
+    var s = 0;
+    for(var i = 0; i < 1000000; i++) {
+        s += i;
+        a.push(i);
+    }
+    return a;
+}
 
-var v = new PersistentVector(0, 5, EMPTY_NODE, []);
-
-time(function() {
-  for(var i = 0; i < 1000000; i++) {
-    v = v.cons(i);
-  }
-  for(var i = 0; i < 1000000; i++) {
-    v.nth_1(999999);
-  }
-});
-
-time(function() {
-  for(var i = 0; i < 1000000; i++) {
-    v.nth_1(999999);
-  }
-});
-
-v = v.cons("foo");
-console.log(v.nth_1(v.count()-1));
-
-var a = [];
-time(function() {
-  for(var i = 0; i < 1000000; i++) {
-    a.push(i);
-  }
-});
-time(function() {
-  for(var i = 0; i < 1000000; i++) {
-    a[999999];
-  }
-});
-*/
+function build_pvec() {
+    var v = EMPTY;
+    for(var i = 0; i < 1000000; i++) {
+        v = v.cons(i);
+    }
+    return v;
+}
+function build_pvec2() {
+    var v = EMPTY2;
+    for(var i = 0; i < 1000000; i++) {
+        v = v.cons(i);
+    }
+    return v;
+}
+//a = time(build_array, "Array built in");
+// pv = time(build_pvec, "Pvec built in");
+// pv2 = time(build_pve2c, "Pvec (no recursion) built in");
